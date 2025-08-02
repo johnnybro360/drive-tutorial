6fead4a2b2a89a8fcbf088afdec23c9896b1b97b
Jonghun Park <abest82@gmail.com>
2025-08-02 12:33:55 +0800

    Update mock data

--- README.md
+++ README.md
@@ -2,7 +2,15 @@
 
 ## TODO
 
-- [ ] Set up database and data model
+- [x] Set up database and data model
 - [ ] Move folder open state to URL
 - [ ] Add auth
 - [ ] Set file uploading
+
+## Note from 02-08-2025
+
+Just finished up connecting database, next steps:
+- [] Update schema to show files and folders
+- [] Manually insert examples
+- [] Render them in the UI
+- [] Push and make sure it all works
---
+++ src/app/file-row.tsx
@@ -0,0 +1,48 @@
+import type { File, Folder } from "~/lib/mock-data"
+import { Folder as FolderIcon, FileIcon } from "lucide-react"
+import Link from "next/link"
+
+export function FileRow(props: { file: File }) {
+    const { file } = props
+
+    return (
+        <li key={file.id} className="px-6 py-4 border-b border-gray-700 hover:bg-gray-750">
+            <div className="grid grid-cols-12 gap-4 items-center">
+                <div className="col-span-6 flex items-center">
+                    <Link href={file.url} className="flex items-center text-gray-100 hover:text-blue-400"
+                    target="_blank">
+                        <FileIcon className="mr-3" size={20} />
+                        {file.name}
+                    </Link>
+                </div>
+                <div className="col-span-3 text-gray-400">{"File"}</div>
+                <div className="col-span-3 text-gray-400">{file.size}</div>
+            </div>
+        </li>
+    )
+}
+
+export function FolderRow(props: { folder: Folder, handleFolderClick: () => void }) {
+    const { folder, handleFolderClick } = props
+
+    return (
+
+        <li key={folder.id} className="px-6 py-4 border-b border-gray-700 hover:bg-gray-750">
+            <div className="grid grid-cols-12 gap-4 items-center">
+                <div className="col-span-6 flex items-center">
+                    {folder.type === "folder" && (
+                        <button
+                            onClick={() => handleFolderClick()}
+                            className="flex items-center text-gray-100 hover:text-blue-400"
+                        >
+                            <FolderIcon className="mr-3" size={20} />
+                            {folder.name}
+                        </button>
+                    ) }
+                </div>
+                <div className="col-span-3 text-gray-400"></div>
+                <div className="col-span-3 text-gray-400"></div>
+            </div>
+        </li>
+    )
+}
